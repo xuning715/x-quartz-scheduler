2773bfb770c7f8ff272b2e8fb45360e9cb02db0e
xuning715@163.com <xuning715@163.com>
2018-04-16 10:13:47 +0800

    sendMsgCarOutOvertimeTrigger

--- src/main/java/com/x/scheduler/job/RpcJob.java
+++ src/main/java/com/x/scheduler/job/RpcJob.java
@@ -10,10 +10,10 @@ import java.lang.reflect.Method;
 
 @Job
 public class RpcJob extends AbstractJob {
-	private final Logger logger = LogManager.getLogger(RpcJob.class);
+    private final Logger logger = LogManager.getLogger(RpcJob.class);
 
-	private BaseRpcService rpcService;
-	private String  methodName;
+    private BaseRpcService rpcService;
+    private String methodName;
     private Class[] paramTypeArray;
     private Object[] paramValueArray;
 
@@ -21,21 +21,23 @@ public class RpcJob extends AbstractJob {
         logger.info("============================RpcJob is inited==============================");
     }
 
-	public void setRpcService(BaseRpcService rpcService) {
-		this.rpcService = rpcService;
-	}
+    public void setRpcService(BaseRpcService rpcService) {
+        this.rpcService = rpcService;
+    }
 
-	public void setMethodName(String methodName) {
-		this.methodName = methodName;
-	}
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
 
-	public void setParamTypeArray(String[] paramTypeArray) throws ClassNotFoundException {
+    public void setParamTypeArray(String[] paramTypeArray) throws ClassNotFoundException {
         this.paramTypeArray = new Class[paramTypeArray.length];
         int i = 0;
-        for (String paramType : paramTypeArray) {
-            this.paramTypeArray[i++] = Class.forName(paramType);
+        if (paramTypeArray != null) {
+            for (String paramType : paramTypeArray) {
+                this.paramTypeArray[i++] = Class.forName(paramType);
+            }
         }
-	}
+    }
 
     public void setParamValueArray(Object[] paramValueArray) {
         this.paramValueArray = paramValueArray;
@@ -44,17 +46,17 @@ public class RpcJob extends AbstractJob {
     private void response() {
 //		ListOperations<String, String> list = redisTemplate.opsForList();
 //		list.leftPush(Constant.MONITOR_QUEUE_ + this.request.getRequestLevel(), JSON.toJSONString(response));
-	}
+    }
 
-	@Override
-	public void execute() {
-		try {
-			Method method = rpcService.getClass().getMethod(methodName, paramTypeArray);
-			Object returnValue = method.invoke(rpcService, paramValueArray);
-		} catch (Exception e) {
-			logger.error(e);
-		}
-		this.response();
-	}
-	
+    @Override
+    public void execute() {
+        try {
+            Method method = rpcService.getClass().getMethod(methodName, paramTypeArray);
+            Object returnValue = method.invoke(rpcService, paramValueArray);
+        } catch (Exception e) {
+            logger.error(e);
+        }
+        this.response();
+    }
+
 }
